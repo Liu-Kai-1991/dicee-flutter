5815160ae5426f7627d504e32c318d4f244a39fa
TheMuellenator <themuellenator@users.noreply.github.com>
2020-01-17 18:16:46 +0000

    Removing android.support dependencies Updating gradle wrapper to 5.4.1-all.zip Updating gradle build to 3.5.3 Adding AndroidX gradle properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.purple,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.purple,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 2;
+  int rightDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              rethrowDice();
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          )),
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              rethrowDice();
+            },
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          )),
+        ],
+      ),
+    );
+  }
+
+  void rethrowDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
